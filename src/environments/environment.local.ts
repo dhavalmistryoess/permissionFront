092a94de337fee4177d468da3440673f0a443e19
krupajethva <krupa.jethwa@theopeneyes.in>
2020-07-24 15:52:50 +0530

    Update environment.local.ts

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost/OESS_Assessment_Frontend',
-  apiUrl: 'http://permission.dev.local/',
+  // baseUrl: 'http://localhost:8080/permissionAPI',
+  // apiUrl: 'http://permission.dev.local/',
+  baseUrl: 'http://localhost:8080/permissionFront',
+  apiUrl: 'http://localhost:8080/permissionAPI/',
   //apiUrl: 'http://assessment-api.devbyopeneyes.com/',
   env: 'local'
 };
